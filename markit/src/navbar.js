bfcb33010b4c76613d8fc8dc514b4953a46f7af7
Kethan Kumar Nasapu <kethan@Kethans-MacBook-Air.local>
2021-07-22 17:04:45 -0300

    Logout functionality

--- markit/src/navbar.js
+++ markit/src/navbar.js
@@ -3,17 +3,43 @@ import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
 import DoneIcon from '@material-ui/icons/Done';
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useHistory } from 'react-router';
 import './App.css';
 
-const NavigationBar = () => {
-    const [toggle, setToggle] = useState(true);
-    const handleToggle = () => {
-        if (toggle) setToggle(false);
-        else setToggle(true);
-    }
-    if (toggle) {
 
+const NavigationBar = () => {
+    const [toggle, setToggle] = useState(false);
+    const [display, setdisplay] = useState(false);
+    const [number, setNumber] = useState(0);
+    const history = useHistory();
+    console.log('Navbar:' + sessionStorage.getItem('markit-email'));
+    console.log(number);
+
+    const increment = () => {
+        if (sessionStorage.getItem('markit-email') == null || sessionStorage.getItem('markit-email') == "") {
+            setNumber(number + 1);
+            setdisplay(false);
+        }
+        else {
+            setdisplay(true);
+        }
+    }
+
+    const handleToggle = () => {
+        console.log('hanndletoggle');
+        if (sessionStorage.getItem('markit-email')) {
+            setdisplay(false);
+            sessionStorage.setItem('markit-email', null);
+            history.goForward('/login');
+        }
+    }
+
+    useEffect(() => {
+        increment();
+    }, [number])
+
+    if (display) {
         return (
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
                 <Container>
@@ -31,7 +57,7 @@ const NavigationBar = () => {
                             <Nav.Link href="#onBoarding">ONBOARDING</Nav.Link>
                             <Nav.Link href="/#/profile">PROFILE</Nav.Link>
                             <Nav.Link href="/#/reset">RESET</Nav.Link>
-                            <Nav.Link href="/#/login" onClick={handleToggle}>LOGOUT</Nav.Link>
+                            <Nav.Link href="#/home" onClick={handleToggle}>LOGOUT</Nav.Link>
                         </Nav>
 
                     </Navbar.Collapse>
@@ -43,7 +69,6 @@ const NavigationBar = () => {
     }
     else {
         return (
-
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
                 <Container>
                     <Navbar.Brand href="#home" style={{ 'color': '#ffc800' }}>
@@ -54,16 +79,13 @@ const NavigationBar = () => {
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="ml-auto">
                             <Nav.Link href="#home">HOME</Nav.Link>
-                            <Nav.Link href="/#/login" onClick={handleToggle}>SIGN IN</Nav.Link>
+                            <Nav.Link href="/#/login" >SIGN IN</Nav.Link>
                         </Nav>
 
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-
-
         );
     }
-
 }
-export default NavigationBar
+export default NavigationBar;
